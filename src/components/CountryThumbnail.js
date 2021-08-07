043e7137b8ffbacb0a6114963304ed3eb874a1ed
Moussa Mamadou <moussa.mamadou@outlook.com>
2021-08-08 01:33:20 +0200

    Update Basic Structure

--- src/components/CountryThumbnail.js
+++ src/components/CountryThumbnail.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 function CountryThumbnail() {
-    return (        
+    return (     
+        <>   
         <div className="country-thumbnail">
             <Link to="/fra" className="country-link">
                 <img src="https://restcountries.eu/data/usa.svg " alt="USA" className="thumbnail-image"/>
@@ -14,6 +15,63 @@ function CountryThumbnail() {
                 </div>
             </Link>
         </div>
+        
+        <div className="country-thumbnail">
+            <Link to="/fra" className="country-link">
+                <img src="https://restcountries.eu/data/usa.svg " alt="USA" className="thumbnail-image"/>
+                <div className="thumbnail-detail">
+                    <h3 className="thumbnail-title">United States of America</h3>
+                    <p>Population: 323,947,000</p>
+                    <p>Region: Americas</p>
+                    <p>Capital: Washington, D.C.</p>                    
+                </div>
+            </Link>
+        </div>
+        <div className="country-thumbnail">
+            <Link to="/fra" className="country-link">
+                <img src="https://restcountries.eu/data/usa.svg " alt="USA" className="thumbnail-image"/>
+                <div className="thumbnail-detail">
+                    <h3 className="thumbnail-title">United States of America</h3>
+                    <p>Population: 323,947,000</p>
+                    <p>Region: Americas</p>
+                    <p>Capital: Washington, D.C.</p>                    
+                </div>
+            </Link>
+        </div>
+        <div className="country-thumbnail">
+            <Link to="/fra" className="country-link">
+                <img src="https://restcountries.eu/data/usa.svg " alt="USA" className="thumbnail-image"/>
+                <div className="thumbnail-detail">
+                    <h3 className="thumbnail-title">United States of America</h3>
+                    <p>Population: 323,947,000</p>
+                    <p>Region: Americas</p>
+                    <p>Capital: Washington, D.C.</p>                    
+                </div>
+            </Link>
+        </div>
+        <div className="country-thumbnail">
+            <Link to="/fra" className="country-link">
+                <img src="https://restcountries.eu/data/usa.svg " alt="USA" className="thumbnail-image"/>
+                <div className="thumbnail-detail">
+                    <h3 className="thumbnail-title">United States of America</h3>
+                    <p>Population: 323,947,000</p>
+                    <p>Region: Americas</p>
+                    <p>Capital: Washington, D.C.</p>                    
+                </div>
+            </Link>
+        </div>
+        <div className="country-thumbnail">
+            <Link to="/fra" className="country-link">
+                <img src="https://restcountries.eu/data/usa.svg " alt="USA" className="thumbnail-image"/>
+                <div className="thumbnail-detail">
+                    <h3 className="thumbnail-title">United States of America</h3>
+                    <p>Population: 323,947,000</p>
+                    <p>Region: Americas</p>
+                    <p>Capital: Washington, D.C.</p>                    
+                </div>
+            </Link>
+        </div>
+        </>
     )
 }
 
