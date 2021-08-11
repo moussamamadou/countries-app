f28bc98b5661ad80fee32164c29953c4e5fa3caf
Moussa Mamadou <moussa.mamadou@outlook.com>
2021-08-11 22:14:51 +0200

    Create 404 Page

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from 'react';;
+
+function NotFoundPage() {
+
+    return (
+        <main>
+            <div className="container">
+                <div className="no-country">☠️ Somebody is lost in no man's land. ☠️</div>
+            </div>            
+        </main>
+    )
+}
+export default NotFoundPage
